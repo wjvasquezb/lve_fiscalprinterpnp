18c91b7b1e5182bf861ecd4dbd9ecfd9cac47931
Victor Suarez <victor.suarez.is@gmail.com>
2016-11-04 20:19:04 +0000

    Script.Spooler.Tfhka.ReporteZ.java editado a trav?s de la web en Bitbucket

--- resources/Bixolon/Windows/Script.Spooler.Tfhka.ReporteZ.java
+++ resources/Bixolon/Windows/Script.Spooler.Tfhka.ReporteZ.java
@@ -3,18 +3,13 @@ import javax.swing.JOptionPane;
 
 String ruta = "C:/IntTFHKA/";
 
-File spooler = new File(ruta+"selectrapos.txt");
-Writer  salida = new BufferedWriter(new FileWriter(spooler));
-salida.write("I0Z\n");
-salida.close();
-
 try {
   //String spoolerBixolon = ruta+"cmd.bat"; 
   //String spoolerBixolon = ruta+"IntTFHKA.exe SendFileCmd(C:/IntTFHKA/selectrapos.txt)";   
   String spoolerBixolon = ruta+"IntTFHKA.exe SendCmd(I0Z)";   
   Runtime.getRuntime().exec(spoolerBixolon,null,new File(ruta)).waitFor();
  } catch (IOException eF) {
-    // Excepciones si hay algÃºn problema al arrancar el ejecutable o al leer su salida.
+    // Excepciones si hay algún problema al arrancar el ejecutable o al leer su salida.
     eF.printStackTrace();
     JOptionPane.showMessageDialog(null, "Error al Imprimir Reporte Z \n" + eF);
  } catch (InterruptedException ex) {
@@ -24,7 +19,7 @@ try {
 FileReader file = new FileReader(ruta+"Status_Error.txt");
 BufferedReader in = new BufferedReader(file);
 String line = in.readLine();  
-String status = line.substring(0,5);
+Object result = line.substring(0,5);
 in.close();
 
-return status.trim();
+return result.trim();
