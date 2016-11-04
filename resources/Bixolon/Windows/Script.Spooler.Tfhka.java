7d56892dbfea58e4aa1663e9c711efd1142f319e
Victor Suarez <victor.suarez.is@gmail.com>
2016-11-04 14:19:48 +0000

    Se mejora Script para Imprimir factura Fiscal desde Windows Script.Spooler.Tfhka.java editado a trav?s de la web en Bitbucket

--- resources/Bixolon/Windows/Script.Spooler.Tfhka.java
+++ resources/Bixolon/Windows/Script.Spooler.Tfhka.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import org.compiere.model.MTax;
 import org.compiere.model.Tax;
 import org.compiere.model.MInvoiceLine;
+import org.compiere.model.MProduct;
 
 public String strpad(String dato, String cformat, int espacio, String pad_type) {
 	tam = dato.length();
@@ -40,75 +41,79 @@ public String strpad(String dato, String cformat, int espacio, String pad_type) 
 }
 
 // Se agregan al Spooler los pagos de la factura
-//public void addPayments(int espacio) {
-//	double efectivo=0.0;
-//	double deb=0.0;
-//	double tc=0.0;
-//	double paper=0.0;
-//	double dif=0.0;
-//	double total_pago=0.0;
-//	double total_invoice=0.0;
-//	for (int j = 0; j < ticket.payments.size(); j++) {
-//	//foreach (PaymentInfo p : ticket.payments)
-//		PaymentInfo p = ticket.payments.get(j) ;
-//	   	if ("cash".equals(p.getName() )) {
-//          	efectivo=efectivo+p.getPaid();
-//     	}   	     	
-//   	  	else if ("magcard".equals(p.getName() )) {
-//        	tc=tc+p.getTotal();
-//     	}
-//    	else if ("paperin".equals(p.getName() )) {
-//			paper=paper+p.getTotal();
-//    	}
-//	}
-//
-//	if (ticket.getTotal() <= efectivo || ticket.getTotal() <= tc || ticket.getTotal() <= paper) {                       
-//        if (ticket.getTotal() <= efectivo) {
-//            salida.write("101\n");
-//        }
-//		else if (ticket.getTotal() <= tc) {
-//            salida.write("109\n");
-//        }
-//		else if (ticket.getTotal() <= paper) {
-//		    salida.write("113\n");
-//        }                        
-//    }	
-//	else {             
-//	    if (efectivo > 0) {
-//		    efect = df1.format(efectivo).toString();
-//            efect = efect.replace(".","");
-//		    efect = strpad(efect, "0", espacio, "STR_PAD_LEFT");
-//            salida.write("201" + efect.replace(",","") + "\n");
-//        }
-//        if (tc > 0) {
-//            tarjeta = df1.format(tc).toString();
-//            tarjeta = tarjeta.replace(".","");
-//		    tarjeta = strpad(tarjeta, "0", espacio, "STR_PAD_LEFT");
-//            salida.write("209" + tarjeta.replace(",","") + "\n");
-//        }
-//        if (paper > 0) {
-//            cestaticket = df1.format(paper).toString();
-//	        cestaticket = cestaticket.replace(".","");
-//		    cestaticket = strpad(cestaticket, "0", espacio, "STR_PAD_LEFT");
-//            salida.write("213" + cestaticket.replace(",","") + "\n");
-//        }
-//		total_pago = efectivo + tc + paper;
-//		total_ticket = ticket.getTotal();
-//		if (total_pago < total_ticket) {
-//            dif = total_ticket - total_pago;
-//		    diferencia = df1.format(dif).toString();
-//            diferencia = diferencia.replace(".","");
-//		    diferencia = strpad(diferencia, "0", espacio, "STR_PAD_LEFT");
-//            salida.write("201" + diferencia.replace(",","") + "\n");
-//        }                        
-//    }
-//}
+public void addPayments(int espacio) {
+	double efectivo=0.0;
+	double deb=0.0;
+	double tc=0.0;
+	double paper=0.0;
+	double dif=0.0;
+	double total_pago=0.0;
+	double total_invoice=0.0;
+	System.out.println("Se agregan los pagos");
+	salida.write("101\n");
+	// for (int j = 0; j < ticket.payments.size(); j++) {
+	// //foreach (PaymentInfo p : ticket.payments)
+	// 	PaymentInfo p = ticket.payments.get(j) ;
+	//    	if ("cash".equals(p.getName() )) {
+ //         	efectivo=efectivo+p.getPaid();
+ //    	}   	     	
+ //  	  	else if ("magcard".equals(p.getName() )) {
+ //       	tc=tc+p.getTotal();
+ //    	}
+ //   	else if ("paperin".equals(p.getName() )) {
+	// 		paper=paper+p.getTotal();
+ //   	}
+	// }
+
+	// if (ticket.getTotal() <= efectivo || ticket.getTotal() <= tc || ticket.getTotal() <= paper) {                       
+ //       if (ticket.getTotal() <= efectivo) {
+ //           salida.write("101\n");
+ //       }
+	// 	else if (ticket.getTotal() <= tc) {
+ //           salida.write("109\n");
+ //       }
+	// 	else if (ticket.getTotal() <= paper) {
+	// 	    salida.write("113\n");
+ //       }                        
+ //   }	
+	// else {             
+	//     if (efectivo > 0) {
+	// 	    efect = df1.format(efectivo).toString();
+ //           efect = efect.replace(".","");
+	// 	    efect = strpad(efect, "0", espacio, "STR_PAD_LEFT");
+ //           salida.write("201" + efect.replace(",","") + "\n");
+ //       }
+ //       if (tc > 0) {
+ //           tarjeta = df1.format(tc).toString();
+ //           tarjeta = tarjeta.replace(".","");
+	// 	    tarjeta = strpad(tarjeta, "0", espacio, "STR_PAD_LEFT");
+ //           salida.write("209" + tarjeta.replace(",","") + "\n");
+ //       }
+ //       if (paper > 0) {
+ //           cestaticket = df1.format(paper).toString();
+	//         cestaticket = cestaticket.replace(".","");
+	// 	    cestaticket = strpad(cestaticket, "0", espacio, "STR_PAD_LEFT");
+ //           salida.write("213" + cestaticket.replace(",","") + "\n");
+ //       }
+	// 	total_pago = efectivo + tc + paper;
+	// 	total_ticket = ticket.getTotal();
+	// 	if (total_pago < total_ticket) {
+ //           dif = total_ticket - total_pago;
+	// 	    diferencia = df1.format(dif).toString();
+ //           diferencia = diferencia.replace(".","");
+	// 	    diferencia = strpad(diferencia, "0", espacio, "STR_PAD_LEFT");
+ //           salida.write("201" + diferencia.replace(",","") + "\n");
+ //       }                        
+ //   }
+}
 
 //Execute Spooler
 public void executeSpooler(String spoolerBixolon) {
 try {
 	//String spoolerBixolon = ruta+"cmd.bat";
+	System.out´.println("Impriemiento Factura Fiscal");
 	Runtime.getRuntime().exec(spoolerBixolon,null,new File(ruta)).waitFor();
+	System.out´.println("Se termina de imprimir la Factura Fiscal");
    } catch (IOException eF)  {
    	 // Excepciones si hay algún problema al arrancar el ejecutable o al leer su salida.
     	eF.printStackTrace();
@@ -131,7 +136,7 @@ salida.write("i01NOMBRE/RAZON SOCIAL: " + BPartner.getName().toUpperCase() + "\n
 salida.write("i02CI/RIF: " + BPartner.getTaxID().toUpperCase() + "\n");
 System.out.println("BUSSINESS PARTNERT " + BPartner.getName().toUpperCase());
 controlNumber=Invoice.get_Value("LVE_controlNumber");
-salida.write("i03" + controlNumber);
+salida.write("i03" + controlNumber + "\n");
 salida.write("@Vendedor: " + SalesRep.toUpperCase() + "\n");
 System.out.println("INVOICE:  " + Invoice.get_Value("LVE_controlNumber"));
 NumberFormat df1 = new DecimalFormat("#0.00"); 
@@ -178,7 +183,7 @@ System.out.println("IVA");
 	//salida.write(producto_cantidad + "\n");
 	
 	if (invoiceLine.getM_Product_ID() != 0) {
-	MProduct product = new MProduct(getCtx(), invoiceLine.getM_Product_ID(), get_TrxName());
+	MProduct product = new MProduct(getCtx, invoiceLine.getM_Product_ID(), get_TrxName);
 	producto = product.getName().toUpperCase();
 	} else 
 		producto = invoiceLine.getDescription();
@@ -192,10 +197,10 @@ System.out.println("IVA");
 salida.write("3\n");
 
 // Se agregan al Spooler los pagos de la factura
-if (Invoice.isPaid()) {
+//if (Invoice.isPaid()) {
 	System.out.println("Factura Tiene Pagos");
-	//addPayments(13);
-}
+	addPayments(13);
+//}
 
 salida.close();
 
